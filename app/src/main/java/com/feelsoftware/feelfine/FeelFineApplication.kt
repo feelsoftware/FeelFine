f925d3763fb9c4d40778c2f49e9f1b7a3ca03e1b
Victor Mykhailiv <victor@feelsoftware.com>
2021-05-26 14:05:42 +0300

    Added Timber dependency

--- app/src/main/java/com/feelsoftware/feelfine/FeelFineApplication.kt
+++ app/src/main/java/com/feelsoftware/feelfine/FeelFineApplication.kt
@@ -2,11 +2,20 @@ package com.feelsoftware.feelfine
 
 import android.app.Application
 import com.feelsoftware.feelfine.di.KoinInit
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class FeelFineApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         KoinInit.init(this)
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 }
