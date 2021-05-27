ba8c66cf55a31aa8a3bdd9ed823225c05829d229
Victor Mykhailiv <victor@feelsoftware.com>
2021-05-27 13:21:13 +0300

    Added Google Fit dependency and permission manager

--- app/src/main/java/com/feelsoftware/feelfine/MainActivity.kt
+++ app/src/main/java/com/feelsoftware/feelfine/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.feelsoftware.feelfine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feelsoftware.feelfine.fit.FitPermissionManager
+import org.koin.android.ext.android.get
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        get<FitPermissionManager>().onPermissionResult(requestCode, resultCode, data)
     }
 }
