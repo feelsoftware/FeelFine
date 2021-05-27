cd8e63d06d4d29704c622054953878ae64a81b91
khrystyna27uz <kristina.sika@aiesec.net>
2021-05-27 11:59:37 +0300

    Added flow for user registration

--- app/src/main/java/com/feelsoftware/feelfine/di/presentation.kt
+++ app/src/main/java/com/feelsoftware/feelfine/di/presentation.kt
@@ -5,8 +5,11 @@ package com.feelsoftware.feelfine.di
 import com.feelsoftware.feelfine.data.repository.UserRepository
 import com.feelsoftware.feelfine.ui.EntryPointViewModel
 import com.feelsoftware.feelfine.ui.MainViewModel
-import com.feelsoftware.feelfine.ui.OnboardingViewModel
 import com.feelsoftware.feelfine.ui.base.EmptyViewModel
+import com.feelsoftware.feelfine.ui.onboarding.AgeViewModel
+import com.feelsoftware.feelfine.ui.onboarding.GenderViewModel
+import com.feelsoftware.feelfine.ui.onboarding.NickNameViewModel
+import com.feelsoftware.feelfine.ui.onboarding.WeightViewModel
 import com.feelsoftware.feelfine.utils.OnBoardingFlowManager
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -21,8 +24,16 @@ val presentationModule = module {
     viewModel {
         MainViewModel(get<UserRepository>())
     }
-
-     viewModel {
-         OnboardingViewModel(get<OnBoardingFlowManager>())
-     }
+    viewModel {
+        NickNameViewModel(get<OnBoardingFlowManager>())
+    }
+    viewModel {
+        GenderViewModel(get<OnBoardingFlowManager>())
+    }
+    viewModel {
+        WeightViewModel(get<OnBoardingFlowManager>())
+    }
+    viewModel {
+        AgeViewModel(get<OnBoardingFlowManager>())
+    }
 }
