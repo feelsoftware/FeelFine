56a6deab854c3356e1c3d7301736f19d9658a9cb
Victor Mykhailiv <victor@feelsoftware.com>
2021-06-08 16:25:52 +0300

    Fetch data from Google Fit

--- app/src/main/java/com/feelsoftware/feelfine/di/fit.kt
+++ app/src/main/java/com/feelsoftware/feelfine/di/fit.kt
@@ -3,11 +3,17 @@
 package com.feelsoftware.feelfine.di
 
 import com.feelsoftware.feelfine.fit.FitPermissionManager
+import com.feelsoftware.feelfine.fit.FitRepository
 import com.feelsoftware.feelfine.fit.GoogleFitPermissionManager
+import com.feelsoftware.feelfine.fit.GoogleFitRepository
 import com.feelsoftware.feelfine.utils.ActivityEngine
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val fitModule = module {
+    single<FitRepository> {
+        GoogleFitRepository(androidContext(), get<FitPermissionManager>())
+    }
     single<FitPermissionManager> {
         GoogleFitPermissionManager(get<ActivityEngine>())
     }
