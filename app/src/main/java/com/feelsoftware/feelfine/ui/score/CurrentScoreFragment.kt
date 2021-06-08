4075478820f2618ce641d081cf87a8aa72f6821b
Victor Mykhailiv <victor@feelsoftware.com>
2021-06-08 18:30:12 +0300

    Display current steps

--- app/src/main/java/com/feelsoftware/feelfine/ui/score/CurrentScoreFragment.kt
+++ app/src/main/java/com/feelsoftware/feelfine/ui/score/CurrentScoreFragment.kt
@@ -1,9 +1,14 @@
 package com.feelsoftware.feelfine.ui.score
 
+import androidx.lifecycle.MutableLiveData
 import com.feelsoftware.feelfine.R
 import com.feelsoftware.feelfine.extension.onClick
+import com.feelsoftware.feelfine.extension.subscribeBy
+import com.feelsoftware.feelfine.fit.usecase.GetFitDataUseCase
+import com.feelsoftware.feelfine.fit.usecase.getCurrentSteps
 import com.feelsoftware.feelfine.ui.base.BaseFragment
 import com.feelsoftware.feelfine.ui.base.BaseViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlinx.android.synthetic.main.fragment_current_score.*
 
@@ -12,10 +17,29 @@ class CurrentScoreFragment : BaseFragment<CurrentScoreViewModel>(R.layout.fragme
     override val viewModel: CurrentScoreViewModel by viewModel()
 
     override fun onReady() {
+        viewModel.stepsData.observe {
+            stepsView.text = "Steps\n$it"
+        }
+
         stepsView.onClick { viewModel.navigate(R.id.toStepScoreFragment) }
         sleepView.onClick { viewModel.navigate(R.id.toSleepScoreFragment) }
         activityView.onClick { viewModel.navigate(R.id.toActivityScoreFragment) }
     }
 }
 
-class CurrentScoreViewModel() : BaseViewModel()
+class CurrentScoreViewModel(
+    useCase: GetFitDataUseCase
+) : BaseViewModel() {
+
+    val stepsData = MutableLiveData<String>()
+    val sleepData = MutableLiveData<String>()
+    val activityData = MutableLiveData<String>()
+
+    init {
+        useCase.getCurrentSteps()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(onSuccess = {
+                stepsData.value = it.count.toString()
+            }).disposeOnInActive()
+    }
+}
