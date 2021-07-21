f3aa0d2427f206690a01ccf15ad0ddeafe9b1e3f
Victor Mykhailiv <victor@feelsoftware.com>
2021-07-21 21:25:58 +0300

    Added Mood enum

---
+++ app/src/main/java/com/feelsoftware/feelfine/data/model/Mood.kt
@@ -0,0 +1,12 @@
+package com.feelsoftware.feelfine.data.model
+
+enum class Mood(val intensity: Int) {
+    APATHY(1),
+    BLAME(2),
+    ANGRY(3),
+    ANXIETY(4),
+    SED(5),
+    NEUTRALITY(6),
+    HAPPY(7),
+    HARMONY(8)
+}
