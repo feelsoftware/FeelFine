3d8f4a4f1116fc30da8dc169f8e0fccfb0b701c4
Victor Mykhailiv <victor@feelsoftware.com>
2021-08-03 11:37:46 +0300

    Display score on UI

--- app/src/main/java/com/feelsoftware/feelfine/ui/score/SleepScoreFragment.kt
+++ app/src/main/java/com/feelsoftware/feelfine/ui/score/SleepScoreFragment.kt
@@ -4,11 +4,13 @@ package com.feelsoftware.feelfine.ui.score
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import com.feelsoftware.feelfine.R
 import com.feelsoftware.feelfine.fit.model.*
 import com.feelsoftware.feelfine.fit.usecase.GetFitDataUseCase
 import com.feelsoftware.feelfine.fit.usecase.getCurrentSleep
 import com.feelsoftware.feelfine.fit.usecase.getPercentSleep
+import com.feelsoftware.feelfine.score.ScoreTargetProvider
 import com.feelsoftware.feelfine.ui.base.BaseFragment
 import com.feelsoftware.feelfine.ui.base.BaseViewModel
 import kotlinx.android.synthetic.main.fragment_sleep_score.*
@@ -21,32 +23,39 @@ class SleepScoreFragment : BaseFragment<SleepScoreViewModel>(R.layout.fragment_s
     override val viewModel: SleepScoreViewModel by viewModel()
 
     override fun onReady() {
-        viewModel.sleepData.observe {
-            // TODO fetch userGoal from main source of personal goals
-            circularProgressBar.progress = it.total.toIntMinutes().applyScore(8 * 60)
-            sleepText.text = resources.getString(R.string.activity_placeholder, it.total.toHours())
+        viewModel.sleepInfo.observe {
             awakeTV.text = "Awake: " + it.awake.toHoursMinutes()
-            lightSleepTV.text =
-                "Light sleep: " + it.lightSleep.toHoursMinutes()
-            deepSleepTV.text =
-                "Deep sleep: " + it.deepSleep.toHoursMinutes()
-            outOfBedTV.text =
-                "Out of bed: " + it.outOfBed.toHoursMinutes()
+            lightSleepTV.text = "Light sleep: " + it.lightSleep.toHoursMinutes()
+            deepSleepTV.text = "Deep sleep: " + it.deepSleep.toHoursMinutes()
+            outOfBedTV.text = "Out of bed: " + it.outOfBed.toHoursMinutes()
         }
-        viewModel.sleepPercents.observe {
+        viewModel.percents.observe {
             scorePercentTV.applyPercentData(it)
+        }
+        viewModel.score.observe {
+            val current = it.currentDuration.hours
+            val target = it.targetDuration.toHours()
+            sleepText.text = "$current / $target\nof your daily goal"
+            circularProgressBar.progress = it.score
         }
         backIV.setOnClickListener { requireActivity().onBackPressed() }
     }
 }
 
-class SleepScoreViewModel(useCase: GetFitDataUseCase) : BaseViewModel() {
+class SleepScoreViewModel(
+    useCase: GetFitDataUseCase,
+    scoreTargetProvider: ScoreTargetProvider
+) : BaseViewModel() {
 
-    val sleepData = MutableLiveData<SleepInfo>()
-    val sleepPercents = MutableLiveData<PercentData>()
+    val sleepInfo = MutableLiveData<SleepInfo>()
+    val percents = MutableLiveData<PercentData>()
+    val score = combineScoreData(
+        sleepInfo.map { it.total.minutesTotal },
+        scoreTargetProvider.getSleepDuration().map { it.minutesTotal }
+    )
 
     init {
-        sleepData.attachSource(useCase.getCurrentSleep()) { it }
-        combinePercentData(sleepPercents, useCase.getPercentSleep())
+        sleepInfo.attachSource(useCase.getCurrentSleep()) { it }
+        combinePercentData(percents, useCase.getPercentSleep())
     }
 }
