73749067676aa1dc5d6e8b6743c99beb09bb4644
Victor Mykhailiv <victor@feelsoftware.com>
2022-11-04 17:24:36 +0200

    Hide DemoProfileBadge by default

--- app/src/main/java/com/feelsoftware/feelfine/ui/profile/DemoProfileBadge.kt
+++ app/src/main/java/com/feelsoftware/feelfine/ui/profile/DemoProfileBadge.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.view.isVisible
 import com.feelsoftware.feelfine.R
 import com.feelsoftware.feelfine.extension.onClick
 import com.feelsoftware.feelfine.ui.dialog.showErrorDialog
@@ -15,6 +16,7 @@ class DemoProfileBadge(context: Context, attrs: AttributeSet) : FrameLayout(cont
 
     init {
         View.inflate(context, R.layout.view_demo_profile_badge, this)
+        isVisible = false
 
         onClick {
             closeable?.close()
