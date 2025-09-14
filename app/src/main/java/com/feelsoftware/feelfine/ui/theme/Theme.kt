cbafe0a85e2d54f4daa58c0e3b72126dc12261a9
Viktor Mykhailiv <vitoksmile@gmail.com>
2025-09-14 11:51:22 +0100

    Forced light theme

--- app/src/main/java/com/feelsoftware/feelfine/ui/theme/Theme.kt
+++ app/src/main/java/com/feelsoftware/feelfine/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.feelsoftware.feelfine.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun FeelFineTheme(
@@ -17,14 +13,16 @@ fun FeelFineTheme(
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    // Dark mode not supported yet
+//    val colorScheme = when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
+    val colorScheme = LightColorScheme
 //    val view = LocalView.current
 //    if (!view.isInEditMode) {
 //        SideEffect {
